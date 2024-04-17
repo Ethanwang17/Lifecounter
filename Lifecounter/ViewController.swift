9698c2e0a05b55d9050edb12786d212eaef35a39
Ethanwang17 <ejw17@uw.edu>
2024-04-16 20:09:02 -0700

    Initial Commit

---
+++ Lifecounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lifecounter
+//
+//  Created by Ethan Wang on 4/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
